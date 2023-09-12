47bd4a60469a1bfb9b327c838310ee1223c94aa3
Jacob Rodriguez <jerd1900@gmail.com>
2023-09-12 10:55:33 -0300

    subida

---
+++ script.js
@@ -0,0 +1,22 @@
+ function showinfo(data){
+    weatherInfo.innerHTML = `
+    <p>Temperatura: <span id="temperature">${data.temperature}</span></p>
+    <p>Descripción del Clima: <span id="description">${data.weather_descriptions}</span></p>
+    <p>Humedad: <span id="humidity">${data.humidity}</span></p>
+    <p>Velocidad del Viento: <span id="windSpeed">${data.wind_speed}</span></p>
+    `
+ }
+
+document.addEventListener("DOMContentLoaded", function(){
+    const btn = document.getElementById("getWeatherButton");
+
+    btn.addEventListener("click",function(){
+        const city = document.getElementById("locationInput").value; 
+        const url = `http://api.weatherstack.com/current?access_key=4c3876398b8d37945ea0ee2b5e4bf628&query=${city}&units=m`
+        
+        fetch(url).then(response => response.json())
+        .then(data => {
+            showinfo(data.current);
+        })
+    });
+});
