980223213aca514a2de1224825993d7b0be8bbb6
Jacob Rodriguez <jerd1900@gmail.com>
2023-11-28 21:33:53 -0300

    estilos

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
- function showinfo(data){
+function showinfo(data) {
     weatherInfo.innerHTML = `
-    <p>Temperatura: <span id="temperature">${data.temperature}</span></p>
-    <p>Descripción del Clima: <span id="description">${data.weather_descriptions}</span></p>
-    <p>Humedad: <span id="humidity">${data.humidity}</span></p>
-    <p>Velocidad del Viento: <span id="windSpeed">${data.wind_speed}</span></p>
+    <div class="card">
+        <div class="card-body">
+        <p>Temperatura: <span id="temperature">${data.temperature}</span></p>
+        <p>Descripción del Clima: <span id="description">${data.weather_descriptions}</span></p>
+        <p>Humedad: <span id="humidity">${data.humidity}</span></p>
+        <p>Velocidad del Viento: <span id="windSpeed">${data.wind_speed}</span></p>
+    </div>
+    </div>    
     `
- }
+}
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     const btn = document.getElementById("getWeatherButton");
 
-    btn.addEventListener("click",function(){
-        const city = document.getElementById("locationInput").value; 
+    btn.addEventListener("click", function () {
+        const city = document.getElementById("locationInput").value;
         const url = `http://api.weatherstack.com/current?access_key=4c3876398b8d37945ea0ee2b5e4bf628&query=${city}&units=m`
-        
+
         fetch(url).then(response => response.json())
-        .then(data => {
-            showinfo(data.current);
-        })
+            .then(data => {
+                showinfo(data.current);
+            })
     });
 });
